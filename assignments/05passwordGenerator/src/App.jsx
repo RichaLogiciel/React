c5e4d8aa44cc505e99d2a97ddf54f34ac4bea389
RichaLogiciel <richa@logiciel.io>
2024-08-21 18:27:29 +0530

    add changes

--- assignments/05passwordGenerator/src/App.jsx
+++ assignments/05passwordGenerator/src/App.jsx
@@ -1,14 +1,95 @@
-import { useState } from 'react'
-import './App.css'
+import { useState,useCallback,useEffect } from 'react'
 
 function App() {
-  const [length setLength] = setState(8)
-  const [numberAllowed setNumberAllowed] = useState(false);
-  const [characterAllowed setAllowedCharacter] = useState(false);
-  const [Password, setPassword] = useState("")
+   const [length, setLength ] = useState(8)
+   const [numberAllowed, setNumberAllowed] = useState(false);
+   const [characterAllowed, setAllowedCharacter] = useState(false);
+   const [password, setPassword] = useState("")
+   const passwordGenerator = useCallback(() => {
+     let pass = ""
+     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+     if(numberAllowed) str += "0123456789"
+     if(characterAllowed) str += "!@#$%^&*()_+"
+     for (let i = 1; i < length; i++) {
+       let char = Math.floor(Math.random() * str.length + 1)
+       pass += str.charAt(char)
+     }
+     setPassword(pass)
+     
+   } ,[setAllowedCharacter,length,setNumberAllowed,setPassword])
+
+   useEffect(() => {
+    passwordGenerator()
+   }, [length,numberAllowed,characterAllowed, passwordGenerator])
+
+  //  passwordGenerator();
   return (
-    <h1 className='text-4xl text-center'>Password Generator</h1>
+    <>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-white bg-gray-700">
+      <h1 className="text-white text-center mb-4 mt-4">Password Generator</h1>
+      <div className="flex shadow-lg text-center bg-gray-400 rounded-lg overflow-hidden mb-5 text-orange-600">
+        <input
+          type="text"
+          value= {password}
+          className="outline-none w-full py-1 px-3"
+          placeholder= "Password"
+          readOnly
+        />
+        <button className=" text-right bg-blue-600 px-3 py-0.5 shrink-0 outline-none">copy</button>
+      </div>
+      <div className="flex text-sm gap-x-2 ">
+        <div className="flex items-center gap-x-1 mb-3">
+          <input 
+          type="range" 
+          min= {8}
+          max= {100}
+          value= {length}
+          className="cursor-pointer" 
+          onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label className="text-blue-300">Length: {length}</label>
+          <div>
+            <input 
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={ () => {
+              setNumberAllowed((prev) => !prev);
+            } }
+             />
+             <label htmlFor="numberInput" className='text-blue-400'>Numbers</label>
+          </div>
+          <div>
+            <input 
+            type="checkbox"
+            defaultChecked={characterAllowed}
+            id="CharInput"
+            onChange={ () => {
+              setAllowedCharacter((prev) => !prev);
+            } }
+             />
+             <label htmlFor="CharInput" className='text-blue-400'>Characters</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
 export default App
+
+
+// <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-white bg-gray-800">
+// <h1 className="text-white text-center">Password Generator</h1>
+// <div className="flex shadow-lg rounded-sm overflow-hidden mb-4">
+//   <input
+//   type="text"
+//   value={password}
+//   className="outline-none w-full py-1 px-3"
+//   placeholder="Password"
+//   readonly
+//   />
+  
+// </div>
+// </div>
--- readme.md
+++ readme.md
@@ -20,3 +20,20 @@ to clean cache:- npm cache clean --force
 command to init tailwind => 
 
 ##onclick ko hmesha ek function chahiye na uski return value na parameter nhi pass kr skte function
+
+<!-- Jo cheez variable hoti h change hoti rehti h uske liye hooks use krte hai means useState lgta hai wha -->
+
+#Hooks
+useState
+useEffect 
+syntax:-
+useEffect(setup,dependencies?)
+useContext
+useCallback => cache memory me data store krlo jitne use krna h krlo 
+syntax:- useCallback(fn, dependencies(in the form of array))
+
+ onChange={ () => {
+              setAllowedCharacter((prev) => !prev);
+            } }
+
+            isme hum arrow function likhte h bcz age fn() => param ni pass kr skte second kyuki hum isme parameter access kr skte hain 
